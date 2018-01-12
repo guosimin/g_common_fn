9380c12c5e9cad699b52a5b3f9b243b821682130
essa1037 <2479186745@qq.com>
2018-01-12 12:08:44 +0800

    复制方法

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ *
+ *
+ * Author:   gsm(qq:865316371)
+ * History:
+ * Date         Version Remarks
+ * ============ ======= ======================================================
+ * 2018/1/12      1.0     First version
+ *
+ * Copyright 2016, all rights reserved. Essa.cn
+ * */
+
+module.exports = {
+    copy:require('./common/copy')
+}
